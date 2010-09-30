2ac1caa3d404b5fbdf83dbf322ce176723f94f8c
Mark Glenn <mglenn@apics.org>
2010-09-30 10:21:33 -0500

    Separated logs for each assembly

--- Apics.Data/Properties/AssemblyInfo.cs
+++ Apics.Data/Properties/AssemblyInfo.cs
@@ -42,3 +42,4 @@ using log4net.Config;
 [assembly: AssemblyFileVersion( "1.0.0.0" )]
 [assembly: CLSCompliant( true )]
 [assembly: XmlConfigurator( ConfigFile = "log4net.config", Watch = true )]
+[assembly: AliasRepository( "Apics.Data" )]
