b182a9f0497860904d3b86c3d34c1e2147a71571
mglenn <devnull@localhost>
2010-10-08 15:04:29 -0500

    Moved around projects

---
+++ Apics.Messaging.Messages/Shipping/ShipmentQuote.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Apics.Messaging.Messages.Shipping
+{
+    public enum NotificationType
+    {
+        Success,
+        Message,
+        Warning,
+        Error,
+        Fatal
+    }
+
+    [DebuggerDisplay( "{Name} - {Price} - {Message}" )]
+    public class ShipmentQuote
+    {
+        #region [ Public Properties ]
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public decimal Price { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public int? Days { get; set; }
+
+        [DataMember]
+        public NotificationType NotificationType { get; set; }
+
+        #endregion [ Public Properties ]
+
+        public ShipmentQuote( string shipmentName, decimal price, int? days, string message = null )
+        {
+            if ( String.IsNullOrEmpty( shipmentName ) )
+                throw new ArgumentException( "Cannot be null or empty", "shipmentName" );
+
+            this.Name = shipmentName;
+            this.Price = price;
+            this.Message = message;
+            this.Days = days;
+            this.NotificationType = NotificationType.Success;
+        }
+
+        public ShipmentQuote( string shipmentName, NotificationType notificationType, string message )
+        {
+            if ( String.IsNullOrEmpty( shipmentName ) )
+                throw new ArgumentException( "shipmentName" );
+            if ( message == null )
+                throw new ArgumentNullException( "shipmentName" );
+
+            this.Name = shipmentName;
+            this.Message = message;
+            this.NotificationType = notificationType;
+        }
+    }
+}
---
+++ Apics.Messaging.Messages/Shipping/ShippingQuoteRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using NServiceBus;
+
+namespace Apics.Messaging.Messages.Shipping
+{
+    public class ShippingQuoteRequest : IMessage
+    {
+        public ShippingAddress Address { get; set; }
+        public decimal Weight { get; set; }
+    }
+
+    public class ShippingAddress
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+
+        public string CountryIsoCode { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
