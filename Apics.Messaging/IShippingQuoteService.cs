14a0864abe137f8b7ddb5bd7e6a5db9a4ef8996f
mglenn <devnull@localhost>
2010-10-12 12:09:27 -0500

    Working WCF service

---
+++ Apics.Messaging/IShippingQuoteService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using Apics.Messaging.Messages.Shipping;
+
+namespace Apics.Messaging
+{
+    [ServiceContract]
+    public interface IShippingQuoteService
+    {
+        [OperationContract]
+        ShipmentQuote[ ] GetShipmentQuotes( ShippingQuoteRequest request );
+    }
+}
