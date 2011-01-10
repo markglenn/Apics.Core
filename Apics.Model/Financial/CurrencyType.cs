7812695bc8e4e263bd0ada0eb0509a8955c74f25
Mark Glenn <markglenn@gmail.com>
2011-01-10 10:31:06 -0600

    Added product pricing models

---
+++ Apics.Model/Financial/CurrencyType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.ActiveRecord;
+using Apics.Model.Location;
+using System.Diagnostics;
+
+namespace Apics.Model.Financial
+{
+    [ActiveRecord( Lazy = true )]
+    [DebuggerDisplay( "CurrencyType: {Id}" )]
+    public class CurrencyType
+    {
+        [PrimaryKey]
+        public virtual int Id { get; set; }
+
+        [Property( NotNull = true, Length = 50 )]
+        public virtual string Name { get; set; }
+
+        [BelongsTo( "CountryID", NotNull = true, Lazy = FetchWhen.OnInvoke )]
+        public virtual Country Country { get; set; }
+
+        [Property( NotNull = true, Length = 5 )]
+        public virtual string CurrencySymbol { get; set; }
+
+        [Property( NotNull = true, Length = 5 )]
+        public virtual string TradingSymbol { get; set; }
+
+        [Property]
+        public virtual int NumDigitsAfterDecimal { get; set; }
+
+        [Property( Length = 50 )]
+        public virtual string FormatString { get; set; }
+
+        [Property( Length = 100 )]
+        public virtual string GeneralLedgerSegment { get; set; }
+
+        [Property( Length = 10 )]
+        public virtual string GeneralLedgerDelimiter { get; set; }
+
+    }
+
+}
---
+++ Apics.Model/User/MemberType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.ActiveRecord;
+using System.Diagnostics;
+
+namespace Apics.Model.User
+{
+    [ActiveRecord( Lazy = true )]
+    [DebuggerDisplay( "MemberType: {Id}" )]
+    public class MemberType
+    {
+        [PrimaryKey]
+        public virtual int Id { get; set; }
+
+        [Property( NotNull = true, Length = 100 )]
+        public virtual string Name { get; set; }
+
+        [BelongsTo( "ParentID", Lazy = FetchWhen.OnInvoke )]
+        public virtual MemberType Parent { get; set; }
+
+        [Property( Length = 255 )]
+        public virtual string Description { get; set; }
+
+        [Property]
+        public virtual bool IsMember { get; set; }
+
+        [Property]
+        public virtual bool IsActive { get; set; }
+
+        [Property( NotNull = true, Length = 100 )]
+        public virtual string DefaultType { get; set; }
+
+    }
+
+}
