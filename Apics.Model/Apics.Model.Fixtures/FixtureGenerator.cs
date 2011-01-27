b900cacb5e4b0ecd130951ff9fcf8d6cf78cc1a5
Mark Glenn <markglenn@gmail.com>
2011-01-27 11:13:59 -0600

    Added FluentValidation and fixture generators for testing. Added extension to Person to get preferred address.

---
+++ Apics.Model/Apics.Model.Fixtures/FixtureGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ploeh.AutoFixture;
+using Apics.Model.User;
+using Apics.Model.Location;
+
+namespace Apics.Model.Fixtures
+{
+    public static class FixtureGenerator
+    {
+        public static Fixture Create( )
+        {
+            var fixture = new Fixture( );
+
+            var homeAddressType = fixture.Build<AddressType>( )
+                .With( t => t.Name, "Home Address" )
+                .CreateAnonymous( );
+
+            var businessAddressType = fixture.Build<AddressType>( )
+                .With( t => t.Name, "Business Address" )
+                .CreateAnonymous( );
+
+            var billingAddressType = fixture.Build<AddressType>( )
+                .With( t => t.Name, "Billing Address" )
+                .CreateAnonymous( );
+
+            fixture.Customize<MemberType>(
+                mt => mt.Without( x => x.Parent )
+            );
+
+            fixture.Customize<Company>( c => c
+                .Without( x => x.ParentCompany )
+                .Without( x => x.RootCompany )
+            );
+
+            fixture.Customize<Person>( p => p
+                .With( x => x.HomeAddress, fixture.Build<Address>( )
+                    .With( a => a.AddressType, homeAddressType )
+                    .CreateAnonymous( ) )
+                .With( x => x.Address, fixture.Build<Address>( )
+                    .With( a => a.AddressType, businessAddressType )
+                    .CreateAnonymous( ) )
+                .With( x => x.BillingAddress, fixture.Build<Address>( )
+                    .With( a => a.AddressType, billingAddressType )
+                    .CreateAnonymous( ) ) );
+
+            return fixture;
+        }
+    }
+}
--- Apics.Model/User/Person.cs
+++ Apics.Model/User/Person.cs
@@ -20,6 +20,16 @@ namespace Apics.Model.User
 
     public class DescribedEnumStringType<T> : NHibernate.Type.EnumStringType<T> where T : struct, IConvertible
     {
+        private readonly IEnumerable<Enum> values;
+        private readonly IDictionary<Enum, string> descriptions;
+
+        public DescribedEnumStringType( )
+        {
+            this.values = Enum.GetValues( typeof( T ) ).Cast<Enum>( );
+
+            this.descriptions = this.values.ToDictionary( v => v, v => v.GetDescription( ) );
+        }
+
         public override object GetValue( object code )
         {
             if ( code == null )
@@ -32,7 +42,18 @@ namespace Apics.Model.User
             return enumeration.GetDescription( );
         }
 
-    } 
+        public override object GetInstance( object code )
+        {
+            if ( code == null )
+                return default( T );
+
+            string enumString = code.ToString( ).Trim( );
+
+            return this.descriptions.Where( v => v.Value == enumString ).Select( v => v.Key )
+                .SingleOrDefault( ) ?? this.values.First( );
+        }
+
+    }
 
     [ActiveRecord( Lazy = true )]
     public class Person
@@ -64,7 +85,7 @@ namespace Apics.Model.User
         [BelongsTo( "CompanyID", Lazy = FetchWhen.OnInvoke )]
         public virtual Company Company { get; set; }
 
-        [BelongsTo("MemberTypeID", Lazy = FetchWhen.OnInvoke)]
+        [BelongsTo( "MemberTypeID", Lazy = FetchWhen.OnInvoke )]
         public virtual MemberType MemberType { get; set; }
 
         [Property( "PreferredAddress", NotNull = true,
@@ -95,5 +116,29 @@ namespace Apics.Model.User
         [Property]
         public virtual DateTime DateUpdated { get; set; }
 
+    }
+
+    public class PersonWithPreferredAddress
+    {
+        public Person Person;
+        public int AddressId;
+    }
+
+    public static class PersonQueries
+    {
+        public static IQueryable<PersonWithPreferredAddress> WithPreferredAddresses( this IEnumerable<Person> people )
+        {
+            return people.AsQueryable( )
+                .Select( p => new PersonWithPreferredAddress
+                {
+                    Person = p,
+                    AddressId = (
+                        p.PreferredAddress == PreferredAddress.Home && p.HomeAddress != null ? p.HomeAddress.Id :
+                        p.PreferredAddress == PreferredAddress.Business && p.Address != null ? p.Address.Id :
+                        p.BillingAddress != null ? p.BillingAddress.Id :
+                        p.HomeAddress != null ? p.HomeAddress.Id :
+                        p.Address != null ? p.Address.Id : -1 )
+                } );
+        }
     }
 }
